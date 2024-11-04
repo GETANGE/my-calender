4ec05834af237f181f5db5eecd6b2987b26e38ff
Getange <emmanuelgetange48@gmail.com>
2024-11-04 20:18:55 +0300

    ADD: Updating & deleting(old) profile picture

--- server/utils/imageBucket.ts
+++ server/utils/imageBucket.ts
@@ -1,4 +1,4 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +8,7 @@ const s3Client = new S3Client({
     endpoint: process.env.AWS_ENDPOINT_URL_S3
 });
 
-export const uploadImage = async (file: any) => {
+export const uploadImage = async (file:any) => {
     try {
         const fileName = `image-${Date.now()}-${file.originalname}`;
 
@@ -22,11 +22,25 @@ export const uploadImage = async (file: any) => {
         await s3Client.send(new PutObjectCommand(uploadParams));
 
         // Construct the image URL
-        const imageUrl = `${process.env.AWS_ENDPOINT_URL_S3}/${"restless-lake-6993"}/${fileName}`;
+        const imageUrl = `${process.env.AWS_ENDPOINT_URL_S3}/restless-lake-6993/${fileName}`;
         
-        return imageUrl; // Returning the full URL to the image
+        return { imageUrl:imageUrl, imageKey: fileName }; // Return URL and key to store in database
     } catch (error) {
         console.log("S3 Upload Error:", error);
         return null; 
     }
 };
+
+export const deleteImage = async (imageKey:string)=>{
+    try {
+        const deleteParams = {
+            Bucket: "restless-lake-6993",
+            Key: imageKey
+        };
+
+        await s3Client.send(new DeleteObjectCommand(deleteParams));
+        console.log("Old image deleted successfully")
+    } catch (error) {
+        console.log("S3 Delete Error:", error);
+    }
+}
